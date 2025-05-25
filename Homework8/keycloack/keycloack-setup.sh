1e489237d48b013ef789894f149566699ce6d0fb
djilanick1 <121696312+djilanick1@users.noreply.github.com>
2025-05-24 21:30:39 -0400

    Create keycloack-setup.sh

---
+++ Homework8/keycloack/keycloack-setup.sh
@@ -0,0 +1,25 @@
+# to be run once Keycloak starts
+#!/bin/bash
+
+# Wait for Keycloak to become available
+until curl -s http://localhost:8080; do
+  echo "Waiting for Keycloak..."
+  sleep 3
+done
+
+echo "Configuring Keycloak..."
+
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
+
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create realms -s realm=demo-realm -s enabled=true
+
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create clients -r demo-realm -s clientId=flask-client -s enabled=true -s publicClient=true -s directAccessGrantsEnabled=true -s redirectUris='["*"]'
+
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create users -r demo-realm -s username=testuser -s enabled=true
+
+USER_ID=$(docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh get users -r demo-realm -q username=testuser --fields id --format csv | tail -n1)
+
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh set-password -r demo-realm --userid $USER_ID --new-password testpassword
+
+echo "Exporting realm configuration..."
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kc.sh export --dir /opt/keycloak/data/import --realm demo-realm --users realm_file
