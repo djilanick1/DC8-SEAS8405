67a8ba111f475d7b4ae779066fca99024aea75ce
djilanick1 <121696312+djilanick1@users.noreply.github.com>
2025-05-26 17:07:47 -0400

    Update keycloack-setup.sh

--- Homework8/keycloack/keycloack-setup.sh
+++ Homework8/keycloack/keycloack-setup.sh
@@ -11,15 +11,15 @@ echo "Configuring Keycloak..."
 
 docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
 
-docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create realms -s realm=demo-realm -s enabled=true
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create realms -s realm=master-realm -s enabled=true
 
-docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create clients -r demo-realm -s clientId=flask-client -s enabled=true -s publicClient=true -s directAccessGrantsEnabled=true -s redirectUris='["*"]'
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create clients -r master-realm -s clientId=flask-client -s enabled=true -s publicClient=true -s directAccessGrantsEnabled=true -s redirectUris='["*"]'
 
-docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create users -r demo-realm -s username=testuser -s enabled=true
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh create users -r master-realm -s username=testuser -s enabled=true
 
-USER_ID=$(docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh get users -r demo-realm -q username=testuser --fields id --format csv | tail -n1)
+USER_ID=$(docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh get users -r master-realm -q username=testuser --fields id --format csv | tail -n1)
 
-docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh set-password -r demo-realm --userid $USER_ID --new-password testpassword
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kcadm.sh set-password -r master-realm --userid $USER_ID --new-password testpassword
 
 echo "Exporting realm configuration..."
-docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kc.sh export --dir /opt/keycloak/data/import --realm demo-realm --users realm_file
+docker exec -it $(docker ps -qf "name=keycloak") /opt/keycloak/bin/kc.sh export --dir /opt/keycloak/data/import --realm master-realm --users realm_file
