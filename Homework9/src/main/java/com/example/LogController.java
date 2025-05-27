75044f288d9d073c3d465af2a1b76a3700bd2c90
djilanick1 <121696312+djilanick1@users.noreply.github.com>
2025-05-27 14:43:10 -0400

    Update LogController.java

--- Homework9/src/main/java/com/example/LogController.java
+++ Homework9/src/main/java/com/example/LogController.java
@@ -3,6 +3,8 @@ package com.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RestController
 public class LogController {
@@ -12,5 +14,12 @@ public class LogController {
     public String logInput(@RequestBody String input) {
         logger.info("User input: " + input);
         return "Logged: " + input;
+    }
+}
+
+@SpringBootApplication
+public class Log4shellDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Log4shellDemoApplication.class, args);
     }
 }
